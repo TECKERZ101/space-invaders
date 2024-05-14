5f040e25d21ce7a9e22302ebc47eb958045b9602
Isaac Clifton <isaac.clifton@teckerz.co.uk>
2024-05-14 20:32:36 +0100

    Added basics

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+# Imports!
 import math
 import random
 import pygame
 from pygame import mixer
+
+# Initialise pygame!
+pygame.init()
+screen = pygame.display.set_mode((800, 500)) # Create screen!
+
+# Background shennanigans!
+background = pygame.image.load('background.png')
+
+# Sound shennanigans!
+# I haven't found a proper background audio yet.
+#mixer.music.load("background.wav")
+#mixer.music.play(-1)
+
+# Window Title & Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
